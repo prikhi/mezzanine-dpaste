228fd321901474ca5f30363c5f96c4b308a466e0
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2014-04-21 08:27:46 -0400

    PyPI Fixup

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 from distutils.core import setup
+from setuptools import find_packages
 
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
@@ -7,8 +8,9 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(
     name='mezzanine-dpaste',
-    version='1.0.0',
-    packages=['mezzpaste'],
+    version='1.0.1',
+    packages=find_packages(),
+    include_package_data=True,
     license='GPLv3',
     description='A Mezzanine app to integrate dpaste, a Django pastebin.',
     long_description=README,
@@ -25,5 +27,5 @@ setup(
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
     ],
-    requires=['dpaste', 'mezzanine'],
+    install_requires=['dpaste', 'mezzanine'],
 )
